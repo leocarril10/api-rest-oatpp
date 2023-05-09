ec4e5117a5d25264a27ebe91616113c148f6c9cd
leocarril10 <125911643+leocarril10@users.noreply.github.com>
2023-05-09 16:02:30 -0600

    Create mongo.sh

---
+++ mongo.sh
@@ -0,0 +1,15 @@
+sudo apt-get install libmongoc-1.0-0
+cd
+
+curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.6.7/mongo-cxx-driver-r3.6.7.tar.gz
+tar -xzf mongo-cxx-driver-r3.6.7.tar.gz
+
+cd mongo-cxx-driver-r3.6.7/build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
+
+sudo cmake --build . --target EP_mnmlstc_core
+
+cmake --build .
+sudo cmake --build . --target install
+ls
