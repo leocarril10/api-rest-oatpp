42d1dcdc5435dc9ec956266b913558981a7cc45f
leocarril10 <125911643+leocarril10@users.noreply.github.com>
2023-05-09 16:27:58 -0600

    Update mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -5,10 +5,16 @@ cd
 
 curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.6.7/mongo-cxx-driver-r3.6.7.tar.gz
 tar -xzf mongo-cxx-driver-r3.6.7.tar.gz
-
 cd mongo-cxx-driver-r3.6.7/build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
+cmake ..                                \
+    -DCMAKE_BUILD_TYPE=Release          \
+    -DCMAKE_INSTALL_PREFIX=/usr/local
+
+cmake ..                                \
+    -DCMAKE_BUILD_TYPE=Release          \
+    -DCMAKE_INSTALL_PREFIX=/opt/mongo   \
+    -DCMAKE_INSTALL_RPATH=/opt/mongo
 
 sudo cmake --build . --target EP_mnmlstc_core
 
